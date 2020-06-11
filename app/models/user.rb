24e26695c66b88529d8a1cf8ba2b31461feeee23
Hilderlan <hilakt9698@gmail.com>
2020-06-11 12:03:06 -0300

    Scaffolds created and Bootstrap cdn added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :timeoutable
+
+  validates_uniqueness_of :name
 end
